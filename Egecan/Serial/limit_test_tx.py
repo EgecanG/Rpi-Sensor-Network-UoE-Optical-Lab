4414c25e7b61ed73797bb449da405b6b0b2c6527
EgecanG <33492683+EgecanG@users.noreply.github.com>
2025-01-03 15:56:59 +0000

    size change

--- Egecan/Serial/limit_test_tx.py
+++ Egecan/Serial/limit_test_tx.py
@@ -15,7 +15,7 @@ def setup_uart_sender():
 
 def send_message(uart):
     # Create test message - "Hello" repeated 200 times
-    test_message = "Hello Tes" * 400000 + "\n"
+    test_message = "Hello Tes" * 40000 + "\n"
     counter = 0
     
     try:
